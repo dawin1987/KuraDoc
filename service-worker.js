abce3cfc7bd9a6ab583b40e7355f11545a7e4020
dawin1987 <dawinalmanzar@gmail.com>
2026-08-16 15:00:56 -0400

    Update service worker to use relative paths

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE = "KuraDoc-cache-v1";
 const FILES = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-azul.png",
-  "/icons/icon-azul-2.png",
-    "/img/loginmedicina1.png",
-      "/assets/app.styles.css",
-         "/assets/app.logic.json"
+  "./",
+  "index.html",
+  "manifest.json",
+  "icons/icon-azul.png",
+  "icons/icon-azul-2.png",
+  "img/loginmedicina1.png",
+  "assets/app.styles.css",
+  "assets/app.logic.json"
 ];
 
 self.addEventListener("install", (event) => {
